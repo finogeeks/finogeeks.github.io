c7bb6ff0475520a7fde606853c5dd65c6d36fe3e
Geeksfino <geeksfino@gmail.com>
2025-09-05 13:59:37 +0800

    remove redundant headers

--- assets/translations.js
+++ assets/translations.js
@@ -2,7 +2,6 @@
 const translations = {
     en: {
       orgDescription: `
-        <h2>FinoGeeks</h2>
         <p>FinoGeeks, established in 2017, is dedicated to helping enterprises build an autonomous and open digital ecosystem under the premise of data security and privacy protection. With our unique enterprise-grade cloud-native mobile application digital engine compatible with mainstream internet mini-program technologies, we provide a foundational platform for enterprise digitization.</p>
         <p>Our plug-in, embedded, lightweight, and flexible product features, along with industry-leading advantages, have empowered industries such as banking, securities, government-enterprise, e-commerce, aviation, parks, and retail. We have formed distinctive innovative application cases in areas like borderless open banking, digital securities, regulatory and compliance technology, e-government, and trusted innovation.</p>
       `,
@@ -11,7 +10,6 @@ const translations = {
     },
     zh: {
       orgDescription: `
-        <h2>凡泰极客</h2>
         <p>凡泰极客成立于 2017 年，致力于帮助企业在数据安全、隐私保护前提下建设自主开放的数字生态，通过其独有的兼容主流互联网小程序技术的企业级云原生移动应用数字引擎为企业数字化提供基础平台。</p>
         <p>凭借我们插件化、嵌入式、轻量而灵活的产品特点及行业领先优势已经赋能银行、证券、政企、电商、航空、园区、零售等多个行业，在无界开放银行、数字券商、监管与合规科技、电子政务、信创等方面形成较有特色的创新应用案例。</p>
       `,
